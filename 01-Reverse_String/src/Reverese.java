7fdc9aaa2afb98399ea09679e0992f677eae7873
Cha1552164 <dchamith4@gmail.com>
2023-06-01 23:20:27 +0530

    method two

--- 01-Reverse_String/src/Reverese.java
+++ 01-Reverse_String/src/Reverese.java
@@ -3,13 +3,24 @@ public class Reverese {
 
         String word ="ABCD";
         String reverse ="";
+//
+//        //method 01
+//        int length =word.length();
+//
+//        for (int i=length-1 ; i>=0; i--){
+//            reverse = reverse + word.charAt(i);
+//        }
+//        System.out.println(reverse);
 
-        //method 01
-        int length =word.length();
+        //method 02
+        char[] array = word.toCharArray();
+        int length =array.length;
 
         for (int i=length-1 ; i>=0; i--){
-            reverse = reverse + word.charAt(i);
+            reverse=reverse+array[i];
         }
         System.out.println(reverse);
+
+
     }
 }
