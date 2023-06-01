02edf2c0ce656abc10deb3e8e43e3b94ddd8884e
Cha1552164 <dchamith4@gmail.com>
2023-06-01 23:37:28 +0530

    method four

--- 01-Reverse_String/src/Reverese.java
+++ 01-Reverse_String/src/Reverese.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Reverese {
     public static void main(String[] args) {
 
@@ -23,9 +28,28 @@ public class Reverese {
 //        System.out.println(reverse);
 
         //method 03
-        StringBuffer stringBuffer=new StringBuffer(word);
-        System.out.println(stringBuffer.reverse());
+//        StringBuffer stringBuffer=new StringBuffer(word);
+//        System.out.println(stringBuffer.reverse());
 
+        //method 04
+        char[] array =word.toCharArray();
 
+        List<Character> list = new ArrayList<>();
+        for (Character character:array
+             ) {list.add(character);
+        }
+        Collections.reverse(list);
+
+        ListIterator listIterator =list.listIterator();
+        while (listIterator.hasNext()){
+
+//            System.out.println(listIterator.next());
+//            D
+//            C
+//            B
+//            A
+
+            System.out.print(listIterator.next());
+        }
     }
 }
