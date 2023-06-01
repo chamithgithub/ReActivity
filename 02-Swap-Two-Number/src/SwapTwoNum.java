dd284c2450f754e2b0233a679ddf0e2fcdba5f8f
Cha1552164 <dchamith4@gmail.com>
2023-06-02 00:03:15 +0530

    SwapTwoNum method 01

---
+++ 02-Swap-Two-Number/src/SwapTwoNum.java
@@ -0,0 +1,14 @@
+public class SwapTwoNum {
+    public static void main(String[] args){
+int a =10;
+int b =20;
+
+        System.out.println("Before Swapping value are a : " +a +" , b :" +b);
+
+        int c =a;
+        a=b;
+        b=c;
+
+        System.out.println("After Swapping value are a : " +a +" , b :" +b);
+    }
+}
