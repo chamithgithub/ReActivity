b67a390f183578d4a89a4d1bf7a8cc09e8ea0e4a
Cha1552164 <dchamith4@gmail.com>
2023-06-02 21:47:14 +0530

    method 02 swap

--- 02-Swap-Two-Number/src/SwapTwoNum.java
+++ 02-Swap-Two-Number/src/SwapTwoNum.java
@@ -1,14 +1,22 @@
 public class SwapTwoNum {
     public static void main(String[] args){
+
+        //method 01
 int a =10;
 int b =20;
 
         System.out.println("Before Swapping value are a : " +a +" , b :" +b);
 
-        int c =a;
-        a=b;
-        b=c;
+//        int c =a;
+//        a=b;
+//        b=c;
+//
+        //method 02
+    a=a+b;
+    b=a-b;
+    a=a-b;
 
         System.out.println("After Swapping value are a : " +a +" , b :" +b);
+
     }
 }
