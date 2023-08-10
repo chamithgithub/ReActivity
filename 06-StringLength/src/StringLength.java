5f2f4a05fc2f11e8cee4473fe74ca6064b8f8eb7
chamithgithub <dchamith4@gmail.com>
2023-08-10 23:13:20 +0530

    module 06 StringLength without method use

--- 06-StringLength/src/StringLength.java
+++ 06-StringLength/src/StringLength.java
@@ -3,6 +3,15 @@ public class StringLength {
 //        find the length of string
         String x ="hello oyata komada";
         System.out.println(x.length());
+
+        //        find the length of string without length method
+        char[] toCharArray = x.toCharArray();
+
+        int length=0;
+        for (Character c: toCharArray){
+            length++;
+        }
+        System.out.println(length);
     }
 
 }
