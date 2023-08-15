fa815846f4478de437d601a0da6ded31934ecabe
chamithgithub <dchamith4@gmail.com>
2023-08-15 23:43:05 +0530

    module 11 FindCountCharactorInString      //using without iteration

--- 11-FindCountCharactorInString/src/FindCountCharactorInString.java
+++ 11-FindCountCharactorInString/src/FindCountCharactorInString.java
@@ -1,22 +1,44 @@
 public class FindCountCharactorInString {
     public static void main(String[] args) {
-        //using iteration
-        String input ="chamithdilshan";
-        char toFind ='h';
+//        //using iteration
+//        String input ="chamithdilshan";
+//        char toFind ='h';
+//
+//        int count =0 ;
+//
+//        input = input.toLowerCase();
+//
+//        for (int i=0; i<input.length();i++){
+//
+//            if (input.charAt(i)==toFind){
+//                count= count+1;
+//            }else {
+//
+//            }
+//        }
+//        System.out.println(toFind +" is present "+count+" Number of times. ");
 
-        int count =0 ;
+        //using without iteration
 
-        input = input.toLowerCase();
+            String input ="chamithdilshan";
+            char toFind ='s';
 
-        for (int i=0; i<input.length();i++){
+            input = input.toUpperCase();
 
-            if (input.charAt(i)==toFind){
-                count= count+1;
-            }else {
+            String chartoFind = Character.toString(toFind).toUpperCase();
 
-            }
-        }
-        System.out.println(toFind +" is present "+count+" Number of times. ");
+            int actualLength = input.length();
+        System.out.println(actualLength);
+
+        input=input.replace(chartoFind,"");
+        int lengthAfterReplcing =input.length();
+
+        System.out.println(lengthAfterReplcing);
+
+
+        System.out.println(actualLength-lengthAfterReplcing);
+
+
 
 
     }
