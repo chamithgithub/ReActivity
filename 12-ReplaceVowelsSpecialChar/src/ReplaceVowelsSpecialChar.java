41c2ea92865f66da4443bf3f73f548fb76344f51
chamithgithub <dchamith4@gmail.com>
2023-08-16 23:49:28 +0530

    module 12 ReplaceVowelsSpecialChar //aproach 02

--- 12-ReplaceVowelsSpecialChar/src/ReplaceVowelsSpecialChar.java
+++ 12-ReplaceVowelsSpecialChar/src/ReplaceVowelsSpecialChar.java
@@ -2,25 +2,28 @@ public class ReplaceVowelsSpecialChar {
     public static void main(String[] args) {
         String givenText = "i like my dream car";
 
-        //aproach 01
+//        //aproach 01
+//
+//        char[] toCharArray = givenText.toCharArray();
+//
+//        for (int i =0; i<givenText.length();i++){
+//            if(givenText.charAt(i)=='A'|| givenText.charAt(i)=='E'|| givenText.charAt(i)=='I'|| givenText.charAt(i)=='O'|| givenText.charAt(i)=='U'
+//                    || givenText.charAt(i)=='a'|| givenText.charAt(i)=='e'|| givenText.charAt(i)=='i'|| givenText.charAt(i)=='o'|| givenText.charAt(i)=='u')
+//
+//                toCharArray[i]='*';
+//        }
+//        for (int i =0; i<givenText.length();i++){
+//            System.out.print(toCharArray[i]);
+//        }
 
-        char[] toCharArray = givenText.toCharArray();
+        //aproach 02
 
-        for (int i =0; i<givenText.length();i++){
-            if(givenText.charAt(i)=='A'|| givenText.charAt(i)=='E'|| givenText.charAt(i)=='I'|| givenText.charAt(i)=='O'|| givenText.charAt(i)=='U'
-                    || givenText.charAt(i)=='a'|| givenText.charAt(i)=='e'|| givenText.charAt(i)=='i'|| givenText.charAt(i)=='o'|| givenText.charAt(i)=='u')
+        String replaceText =givenText.replaceAll("[AEIOUaeiou]","*");
+        System.out.println(replaceText);
 
-                toCharArray[i]='*';
-        }
-        for (int i =0; i<givenText.length();i++){
-            System.out.print(toCharArray[i]);
-        }
+    }
 
-        }
-
-
-
-        }
+}
 
 
 
