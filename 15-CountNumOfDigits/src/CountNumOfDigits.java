2b4e6fadc8aeb427edc4c2eb9009d631c140554d
chamithgithub <dchamith4@gmail.com>
2023-08-19 23:01:46 +0530

    module 15 CountNumOfDigits

---
+++ 15-CountNumOfDigits/src/CountNumOfDigits.java
@@ -0,0 +1,13 @@
+public class CountNumOfDigits {
+    public static void main(String[] args) {
+
+        int num=1234;
+        int count=0;
+
+        while (num>0){
+            num=num/10;
+            count++;
+        }
+        System.out.println("number of digits :"+count);
+    }
+}
