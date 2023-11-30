510c398ca5d14838f54055638ef62f8460401441
chamithgithub <dchamith4@gmail.com>
2023-11-30 20:52:54 +0530

    _25_FactorialOfANumber

---
+++ _25_FactorialOfANumber/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author A.C.Dilshan
+ */
+public class Main {
+    public static void main(String[] args) {
+        int num =5;
+        int factorial=1;
+
+        for(int i=1;i<=num ;i++ ){
+            factorial =factorial*i;
+
+        }
+        System.out.println("factorial number is "+factorial);
+    }
+}
