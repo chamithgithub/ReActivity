3281580eb568fcbd1bca8b66a14c21626ecba138
chamithgithub <dchamith4@gmail.com>
2023-12-04 12:02:43 +0530

    24 | Sort Elements in Array Using Bubble Sort

--- _24_SortElementsInArrayUsingBubbleSort/src/Main.java
+++ _24_SortElementsInArrayUsingBubbleSort/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("before"+Arrays.toString(ar));
 
         for (int i=0;i<n-1;i++) {
-
+  
             for (int j=0;j<n-1;j++) {
               if (ar[j]>ar[j+1]){
                     int temp=ar[j];
