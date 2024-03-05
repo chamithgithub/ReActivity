0f7fc654ddea55760d96c17760af1f8e97e1d943
chamithgithub <dchamith4@gmail.com>
2024-03-05 12:07:37 +0530

    update

---
+++ 01-Reverse_String/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author A.C.Dilshan
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        int input1 =56;
+        int input2 =65;
+
+
+        System.out.println("total :"+(input1+input2));
+
+    }
+}
--- _24_SortElementsInArrayUsingBubbleSort/src/Main.java
+++ _24_SortElementsInArrayUsingBubbleSort/src/Main.java
@@ -4,24 +4,45 @@ import java.util.Arrays;
  * @author A.C.Dilshan
  */
 public class Main {
-    public static void main(String[] args) {
-        int ar [] ={5,1,4,2,8};
+//    public static void main(String[] args) {
+//        int ar [] ={5,1,4,2,8};
+//
+//        int n =ar.length;
+//
+//        System.out.println("before"+Arrays.toString(ar));
+//
+//        for (int i=0;i<n-1;i++) {
+//
+//            for (int j=0;j<n-1;j++) {
+//              if (ar[j]>ar[j+1]){
+//                    int temp=ar[j];
+//                    ar[j]=ar[j+1];
+//                    ar[j+1]=temp;
+//              }
+//            }
+//
+//        }
+//        System.out.println("after"+Arrays.toString(ar));
+//    }
+public static void main(String[] args) {
 
-        int n =ar.length;
+   int  ar[] ={5,2,3,4,7,6};
+   int temp=0;
 
-        System.out.println("before"+Arrays.toString(ar));
+    System.out.println("before"+Arrays.toString(ar));
 
-        for (int i=0;i<n-1;i++) {
-  
-            for (int j=0;j<n-1;j++) {
-              if (ar[j]>ar[j+1]){
-                    int temp=ar[j];
-                    ar[j]=ar[j+1];
-                    ar[j+1]=temp;
-              }
-            }
+   for (int i=0;i< ar.length-1;i++){
+       for (int j=0;j<ar.length-1;j++){
+           if (ar[j]>ar[j+1]) {
+           temp=ar[j];
+           ar[j]=ar[j+1];
+           ar[j+1]=temp;
+           }
+           }
+       }
+    System.out.println("after"+Arrays.toString(ar));
+   }
 
-        }
-        System.out.println("after"+Arrays.toString(ar));
-    }
+
 }
+
